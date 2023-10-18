8c4452bfc0c9e7032160912620560f8bf08d3c9e
Anand Chowdhary <mail@anandchowdhary.com>
2023-10-18 14:54:43 +0200

    chore(app): Add SEO icons, image

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -41,6 +41,30 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://api.fontshare.com/v2/css?f[]=cabinet-grotesk@800,500,700&display=swap"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta property="og:image" content="/social.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1920" />
+        <meta property="og:image:height" content="1080" />
       </head>
       <body className="flex min-h-full flex-col">
         {children}
